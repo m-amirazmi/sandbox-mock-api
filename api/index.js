bebbb4932ca7ed6f2578b88bf40459ebe60089e2
Muhamad Amir <m.amirazmi@hotmail.com>
2024-10-22 11:07:02 +0800

    available on GET too

--- api/index.js
+++ api/index.js
@@ -4,93 +4,99 @@ const app = express();
 
 app.use(cors());
 
+const list = [
+  {
+    id: 26,
+    bankKey: "MBBEMYKL",
+    bankName: "Malayan Banking Berhad",
+    accountNoLength: "12",
+    isEnabled: true,
+  },
+  {
+    id: 32,
+    bankKey: "PHBMMYKL",
+    bankName: "Affin Bank Berhad",
+    accountNoLength: "12",
+    isEnabled: true,
+  },
+  {
+    id: 3,
+    bankKey: "AIBBMYKL",
+    bankName: "Affin Islamic Bank Berhad",
+    accountNoLength: "12",
+    isEnabled: true,
+  },
+  {
+    id: 36,
+    bankKey: "RJHIMYKL",
+    bankName: "Al Rajhi Banking & Investment",
+    accountNoLength: "15",
+    isEnabled: true,
+  },
+  {
+    id: 27,
+    bankKey: "MFBBMYKL",
+    bankName: "Alliance Bank Berhad",
+    accountNoLength: "15",
+    isEnabled: true,
+  },
+  {
+    id: 5,
+    bankKey: "ALSRMYK1",
+    bankName: "Alliance Islamic Bank Berhad",
+    accountNoLength: "15",
+    isEnabled: true,
+  },
+  {
+    id: 6,
+    bankKey: "ARBKMYKL",
+    bankName: "Ambank Berhad",
+    accountNoLength: "13,12",
+    isEnabled: true,
+  },
+  {
+    id: 4,
+    bankKey: "AISLMYKL",
+    bankName: "AmIslamic Bank Berhad",
+    accountNoLength: "13",
+    isEnabled: true,
+  },
+  {
+    id: 7,
+    bankKey: "BIMBMYKL",
+    bankName: "Bank Islam Malaysia",
+    accountNoLength: "14",
+    isEnabled: true,
+  },
+  {
+    id: 9,
+    bankKey: "BMMBMYKL",
+    bankName: "Bank Muamalat (Malaysia)",
+    accountNoLength: "14,17",
+    isEnabled: true,
+  },
+  {
+    id: 11,
+    bankKey: "BOFAMY2X",
+    bankName: "Bank Of America",
+    accountNoLength: "12",
+    isEnabled: true,
+  },
+  {
+    id: 12,
+    bankKey: "BOTKMYKX",
+    bankName: "Bank Of Tokyo Mitsubishi (M) B",
+    accountNoLength: "6",
+    isEnabled: true,
+  },
+];
+
 app.post("/api/banks", (req, res) => {
-  return res.json([
-    {
-      id: 26,
-      bankKey: "MBBEMYKL",
-      bankName: "Malayan Banking Berhad",
-      accountNoLength: "12",
-      isEnabled: true,
-    },
-    {
-      id: 32,
-      bankKey: "PHBMMYKL",
-      bankName: "Affin Bank Berhad",
-      accountNoLength: "12",
-      isEnabled: true,
-    },
-    {
-      id: 3,
-      bankKey: "AIBBMYKL",
-      bankName: "Affin Islamic Bank Berhad",
-      accountNoLength: "12",
-      isEnabled: true,
-    },
-    {
-      id: 36,
-      bankKey: "RJHIMYKL",
-      bankName: "Al Rajhi Banking & Investment",
-      accountNoLength: "15",
-      isEnabled: true,
-    },
-    {
-      id: 27,
-      bankKey: "MFBBMYKL",
-      bankName: "Alliance Bank Berhad",
-      accountNoLength: "15",
-      isEnabled: true,
-    },
-    {
-      id: 5,
-      bankKey: "ALSRMYK1",
-      bankName: "Alliance Islamic Bank Berhad",
-      accountNoLength: "15",
-      isEnabled: true,
-    },
-    {
-      id: 6,
-      bankKey: "ARBKMYKL",
-      bankName: "Ambank Berhad",
-      accountNoLength: "13,12",
-      isEnabled: true,
-    },
-    {
-      id: 4,
-      bankKey: "AISLMYKL",
-      bankName: "AmIslamic Bank Berhad",
-      accountNoLength: "13",
-      isEnabled: true,
-    },
-    {
-      id: 7,
-      bankKey: "BIMBMYKL",
-      bankName: "Bank Islam Malaysia",
-      accountNoLength: "14",
-      isEnabled: true,
-    },
-    {
-      id: 9,
-      bankKey: "BMMBMYKL",
-      bankName: "Bank Muamalat (Malaysia)",
-      accountNoLength: "14,17",
-      isEnabled: true,
-    },
-    {
-      id: 11,
-      bankKey: "BOFAMY2X",
-      bankName: "Bank Of America",
-      accountNoLength: "12",
-      isEnabled: true,
-    },
-    {
-      id: 12,
-      bankKey: "BOTKMYKX",
-      bankName: "Bank Of Tokyo Mitsubishi (M) B",
-      accountNoLength: "6",
-      isEnabled: true,
-    },
-  ]);
+  return res.json(list);
+});
+
+app.get("/api/banks", (req, res) => {
+  return res.json(list);
 });
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
